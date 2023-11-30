11ebd6fb66f8910824c294d80e673048f0286227
Siyuan Peng @ WS <peng2000@umd.edu>
2023-11-30 11:46:13 -0500

    add utils files

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,6 +2,8 @@ import torch
 from torch.utils.data import Dataset
 from pathlib import Path
 import pandas as pd
+# import util functions from ./utils/utils.py
+from utils.utils import euler_angles_to_matrix, rotation_6d_to_matrix
 
 class FallingData(Dataset):
     def __init__(self, data_path):
@@ -22,10 +24,27 @@ class FallingData(Dataset):
         path = self.data_path[idx]
         trial_number = int(path.stem.split('_')[1])
         data = pd.read_csv(path)
+        label = self.label[self.label['Trial Number'] == trial_number]
+        arm_rot = torch.tensor(data[["arm_loc_x","arm_loc_y","arm_loc_z"]].values)
+        print(f'length of this action: {len(data)}')
+        # euler angles to rotation matrix
+        arm_rot = euler_angles_to_matrix(arm_rot, "XYZ")
+        # rotation matrix to 6D representation
+        arm_rot = rotation_6d_to_matrix(arm_rot)
+        print(f'arm_rot: {arm_rot.shape}')
+
+
         data_dict = {
             "armature_rotation": torch.tensor(data[["arm_rot_x","arm_rot_y","arm_rot_z"]].values),
             "armature_location": torch.tensor(data[["arm_loc_x","arm_loc_y","arm_loc_z"]].values),
             "joint_rotation": torch.tensor(data.loc[:, "Pelvis_x":"R_Hand_z"].values),
+            "label": torch.tensor((label.values)[0, 1:]) # remove the first column, which is the trial number
         }
         
-        return data
+        return data_dict
+    
+# test dataset works
+data_path = "/home/siyuan/research/PoseFall/data/MoCap/Mocap_processed_data"
+dataset = FallingData(data_path)
+# print(f'length of dataset: {len(dataset)}')
+print(f'first item in dataset: {dataset[0]}')
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import functools
 
 def rotation_6d_to_matrix(d6: torch.Tensor) -> torch.Tensor:
     """
@@ -35,9 +36,72 @@ def matrix_to_rotation_6d(matrix: torch.Tensor) -> torch.Tensor:
     Returns:
         6D rotation representation, of size (*, 6)
 
-    [1] Zhou, Y., Barnes, C., Lu, J., Yang, J., & Li, H.
+    Zhou, Y., Barnes, C., Lu, J., Yang, J., & Li, H.
     On the Continuity of Rotation Representations in Neural Networks.
     IEEE Conference on Computer Vision and Pattern Recognition, 2019.
     Retrieved from http://arxiv.org/abs/1812.07035
     """
     return matrix[..., :2, :].clone().reshape(*matrix.size()[:-2], 6)
+
+
+def _axis_angle_rotation(axis: str, angle):
+    """
+    Return the rotation matrices for one of the rotations about an axis
+    of which Euler angles describe, for each value of the angle given.
+
+    Args:
+        axis: Axis label "X" or "Y or "Z".
+        angle: any shape tensor of Euler angles in radians
+
+    Returns:
+        Rotation matrices as tensor of shape (..., 3, 3).
+
+    Petrovich, M., Black, M. J., &amp; Varol, G. (2021). 
+    Action-conditioned 3D human motion synthesis with Transformer Vae. 
+    2021 IEEE/CVF International Conference on Computer Vision (ICCV). 
+    Retrived from https://doi.org/10.1109/iccv48922.2021.01080 
+    """
+
+    cos = torch.cos(angle)
+    sin = torch.sin(angle)
+    one = torch.ones_like(angle)
+    zero = torch.zeros_like(angle)
+
+    if axis == "X":
+        R_flat = (one, zero, zero, zero, cos, -sin, zero, sin, cos)
+    if axis == "Y":
+        R_flat = (cos, zero, sin, zero, one, zero, -sin, zero, cos)
+    if axis == "Z":
+        R_flat = (cos, -sin, zero, sin, cos, zero, zero, zero, one)
+
+    return torch.stack(R_flat, -1).reshape(angle.shape + (3, 3))
+
+
+def euler_angles_to_matrix(euler_angles, convention: str):
+    """
+    Convert rotations given as Euler angles in radians to rotation matrices.
+
+    Args:
+        euler_angles: Euler angles in radians as tensor of shape (..., 3).
+        convention: Convention string of three uppercase letters from
+            {"X", "Y", and "Z"}.
+
+    Returns:
+        Rotation matrices as tensor of shape (..., 3, 3).
+        
+    Petrovich, M., Black, M. J., &amp; Varol, G. (2021). 
+    Action-conditioned 3D human motion synthesis with Transformer Vae. 
+    2021 IEEE/CVF International Conference on Computer Vision (ICCV). 
+    Retrived from https://doi.org/10.1109/iccv48922.2021.01080 
+    """
+    if euler_angles.dim() == 0 or euler_angles.shape[-1] != 3:
+        raise ValueError("Invalid input euler angles.")
+    if len(convention) != 3:
+        raise ValueError("Convention must have 3 letters.")
+    if convention[1] in (convention[0], convention[2]):
+        raise ValueError(f"Invalid convention {convention}.")
+    for letter in convention:
+        if letter not in ("X", "Y", "Z"):
+            raise ValueError(f"Invalid letter {letter} in convention string.")
+    matrices = map(_axis_angle_rotation, convention, torch.unbind(euler_angles, -1))
+    return functools.reduce(torch.matmul, matrices)
