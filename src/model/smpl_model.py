3f50d6deda0aa8729210f3d0a50e172dc12578bd
Siyuan Peng @ WS <peng2000@umd.edu>
2023-12-10 16:03:11 -0500

    finish vertex generate

--- src/model/smpl_model.py
+++ src/model/smpl_model.py
@@ -2,7 +2,10 @@ from smplx import SMPLLayer
 import torch.nn as nn
 from data_processing.utils import parse_output
 from icecream import ic
+from data_processing.utils import rotation_6d_to_matrix
+import torch
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class SMPLModel(nn.Module):
     """
@@ -14,18 +17,21 @@ class SMPLModel(nn.Module):
         self.human_model = SMPLLayer(
             model_path = SMPL_model_path,
             creat_body_pose=True,
-        )
+        ).to(DEVICE)
 
     def forward(self, batch):
         """
         we discard the locationa and rotation of the armature. 
         Only calculate the vertex transformation resulted from joint rotation
         """
-
         parsed_output = parse_output(batch)
         bone_rot = parsed_output["bone_rot"]
-        ic(bone_rot.shape)
+        batch_size = bone_rot.size(0)
         # convert continuous rotation representationt to rotation matrix
-        
-
-        exit()
+        bone_rot = rotation_6d_to_matrix(bone_rot)
+        # body pose shape requirement: BxJx3x3
+        body_pose = bone_rot.reshape(-1, 24, 3, 3)[:, 1:, :, :]
+        SMPL_output = self.human_model(body_pose = body_pose, return_verts=True)
+        verts = SMPL_output.vertices
+        verts = verts.reshape(batch_size, -1, verts.size(-2), verts.size(-1))
+        return verts
