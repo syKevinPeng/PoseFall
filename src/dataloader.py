fix untracable gradient problem

src/dataloader.py
@@ -83,7 +83,7 @@ class FallingData(Dataset):
             arm_loc = torch.cat((arm_loc, torch.zeros((pad_length, 3))))
             bone_rot = torch.cat((bone_rot, torch.zeros((pad_length, 24, 6))))
 
-            # prepare padding mask for the data: attend zero position
+            # prepare padding mask for the data: attend zero position and ignore zeros
             # TODO: simplify the padding mask
             src_key_padding_mask = torch.cat(
                 (torch.zeros((frame_length)), torch.ones((pad_length)))

src/model/CVAE.py
@@ -49,12 +49,15 @@ class CAVE(nn.Module):
     
     def compute_loss(self, batch):
         pred_batch = batch["output"]
-        input_batch = batch["data"]
+        input_batch = torch.clone(batch["data"])
         mask_batch = batch["mask"]
 
         padding = mask_batch.bool().unsqueeze(-1).expand(-1, -1, pred_batch.size(-1))
         input_batch[padding] = 0
         pred_batch[padding] = 0
+
+        ic(input_batch)
+        ic(mask_batch)
 
         # human model param l2 loss
         human_model_loss = human_param_loss(pred_batch, input_batch)

src/model/model.py
@@ -45,7 +45,7 @@ class Encoder(nn.Module):
         dim_feedforward=1024,
         dropout=0.1,
         activation="gelu",
-    ):
+    ): 
         super(Encoder, self).__init__()
         self.latent_dim = latent_dim  # latent dimension
         self.num_classes = num_classes
@@ -102,13 +102,13 @@ class Encoder(nn.Module):
         # add mu and sigma queries to the input
         xseq = torch.cat((muQuery, sigmaQuery, x), dim=1)
         # add positional encoding
-        xseq = self.pos_encoder(xseq) 
+        encoded_xseq = self.pos_encoder(xseq) 
         
         # create a bigger mask to attend to mu and sigma
         extra_mask = torch.zeros((batch_size, 2)).to(mask.device)
         mask_seq = torch.cat((extra_mask, mask), dim=1)
         
-        encoder_output = self.trans_encoder(xseq, src_key_padding_mask=mask_seq.bool())
+        encoder_output = self.trans_encoder(encoded_xseq, src_key_padding_mask=mask_seq.bool())
         # get the first two output
         mu = encoder_output[:, 0, :]
         sigma = encoder_output[:, 1, :]
@@ -152,22 +152,22 @@ class Decoder(nn.Module):
         latent_dim = z.size(1)
         batch_size, num_frames = mask.shape
         # shift the latent noise vector to be the action noise
-        z = z + y @ self.action_biases
+        shifted_z = z + y @ self.action_biases
         # z is sequence of size 1
-        z = z.unsqueeze(1)
+        shifted_z = shifted_z.unsqueeze(1)
         # time queries
-        timequeries = torch.zeros(batch_size, num_frames, latent_dim, device=z.device)
+        timequeries = torch.zeros(batch_size, num_frames, latent_dim, device=shifted_z.device)
         # sequence encoding
         timequeries = self.pos_encoder(timequeries)
 
         # decode
-        decoder_output = self.trans_decoder(tgt = timequeries, memory=z, tgt_key_padding_mask=mask.bool())
+        decoder_output = self.trans_decoder(tgt = timequeries, memory=shifted_z, tgt_key_padding_mask=mask.bool())
         # get output sequences
         output = self.final_layer(decoder_output).reshape(batch_size, num_frames, -1)
 
         # setting zero for padded area
         # expand the mask to the output size
-        padding = mask.bool().unsqueeze(-1).expand(-1, -1, output.size(-1))
-        output[padding] = 0
+        # padding = mask.bool().unsqueeze(-1).expand(-1, -1, output.size(-1))
+        # output[padding] = 0
         batch["output"] = output
         return batch

src/train.py
@@ -7,12 +7,24 @@ from model.CVAE import CAVE
 from icecream import ic
 
 # a simple train loop for development
+torch.autograd.set_detect_anomaly(True)
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 data = FallingData("/home/siyuan/research/PoseFall/data/MoCap/Mocap_processed_data")
 dataloaders = torch.utils.data.DataLoader(data, batch_size=1, shuffle=True, num_workers=0)
+
+# define optimizer 
+num_class = 9
+# num_class = impa_label.size(1)
+# initialize the encoder
+encoder = Encoder(num_classes=num_class).to(DEVICE)
+decoder  = Decoder(num_classes=num_class).to(DEVICE)
+model = CAVE(encoder, decoder).to(DEVICE)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
 for i_batch, (data_dict) in enumerate(dataloaders):
+      optimizer.zero_grad()
       # labels
       impa_label = data_dict['impa_label'].to(DEVICE)
       glit_label = data_dict['glit_label'].to(DEVICE)
@@ -43,11 +55,7 @@ for i_batch, (data_dict) in enumerate(dataloaders):
 
       # build impact only
       # construct the transformer
-      num_class = impa_label.size(1)
-      # initialize the encoder
-      encoder = Encoder(num_classes=num_class).to(DEVICE)
-      decoder  = Decoder(num_classes=num_class).to(DEVICE)
-      model = CAVE(encoder, decoder).to(DEVICE)
+
       batch = {
             'data': impact_poses, 
             'label': impa_label, 
@@ -55,4 +63,6 @@ for i_batch, (data_dict) in enumerate(dataloaders):
             "lengths": impact_length}
       batch = model(batch = batch)
       loss = model.compute_loss(batch)
-      break
+      loss.backward()
+      optimizer.step()
+      ic(loss.item())