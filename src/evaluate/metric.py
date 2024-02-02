752e6e657eba854f760eda88e9a2335c4a138799
Siyuan Peng @ WS <peng2000@umd.edu>
2024-02-02 12:47:55 -0500

    add evaluation model

--- src/evaluate/metric.py
+++ src/evaluate/metric.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy import linalg
-
+import torch
 
 def calculate_activation_statistics(activations):
     """Calculation of the statistics used by the FID.
@@ -68,3 +68,67 @@ def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
     tr_covmean = np.trace(covmean)
 
     return (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean)
+
+def calculate_fid(statistics_1, statistics_2):
+    return calculate_frechet_distance(statistics_1[0], statistics_1[1],
+                                      statistics_2[0], statistics_2[1])
+
+
+def calculate_accuracy(model, motion_loader, num_labels, classifier, device):
+    confusion = torch.zeros(num_labels, num_labels, dtype=torch.long)
+    with torch.no_grad():
+        for batch in motion_loader:
+            batch_prob = classifier(batch)["yhat"]
+            batch_pred = batch_prob.max(dim=1).indices
+            for label, pred in zip(batch["y"], batch_pred):
+                confusion[label][pred] += 1
+
+    accuracy = torch.trace(confusion)/torch.sum(confusion)
+    return accuracy.item(), confusion
+
+
+import torch
+import numpy as np
+
+
+# from action2motion
+def calculate_diversity_multimodality(activations, labels, num_labels):
+    diversity_times = 200
+    multimodality_times = 20
+    labels = labels.long()
+    num_motions = len(labels)
+
+    diversity = 0
+        
+    first_indices = np.random.randint(0, num_motions, diversity_times)
+    second_indices = np.random.randint(0, num_motions, diversity_times)
+    for first_idx, second_idx in zip(first_indices, second_indices):
+        diversity += torch.dist(activations[first_idx, :],
+                                activations[second_idx, :])
+    diversity /= diversity_times
+
+    multimodality = 0
+    label_quotas = np.repeat(multimodality_times, num_labels)
+    while np.any(label_quotas > 0):
+        # print(label_quotas)
+        first_idx = np.random.randint(0, num_motions)
+        first_label = labels[first_idx]
+        if not label_quotas[first_label]:
+            continue
+
+        second_idx = np.random.randint(0, num_motions)
+        second_label = labels[second_idx]
+        while first_label != second_label:
+            second_idx = np.random.randint(0, num_motions)
+            second_label = labels[second_idx]
+
+        label_quotas[first_label] -= 1
+
+        first_activation = activations[first_idx, :]
+        second_activation = activations[second_idx, :]
+        multimodality += torch.dist(first_activation,
+                                    second_activation)
+
+    multimodality /= (multimodality_times * num_labels)
+
+    return diversity.item(), multimodality.item()
